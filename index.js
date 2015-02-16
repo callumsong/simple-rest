40eb28123bd5b1804b4389044d91abe6dfb64365
kevintechie <kevin@gabu.com>
2015-02-15 21:36:34 -0800

    everything seems to work

--- index.js
+++ index.js
@@ -5,15 +5,37 @@
 
 var server = require('./lib/server');
 var router = require('./lib/router');
+var util = require('./lib/util');
 
-function testRouteHandler(request, response, indexParam) {
-	response.writeHead(200, {'Content-Type': 'text/plain'});
-	response.write('hello world');
-	response.end();
-	console.log('hello world ' + indexParam);
+// Demo request handler for GET requests.
+function testGetRouteHandler(request, response, indexParam) {
+	var message = {};
+	message.msg = 'hello world';
+	message.indexParam = indexParam;
+	util.writeSuccess(response, JSON.stringify(message));
 }
 
-router.addRoute('/', 'get', testRouteHandler);
-router.addRoute('/burp', 'get', testRouteHandler);
+// Demo request handler for PATCH requests.
+function testPatchRouteHandler(request, response, indexParam) {
+	util.getJsonBody(request, response, function (body) {
+		body.msg = 'hello world';
+		body.indexParam = indexParam;
+		util.writeSuccess(response, JSON.stringify(body));
+	});
+}
 
+/*
+* Add a route to request handler map with the addRoute method
+* The first parameter is the base route.
+* The second parameter is the request method.
+* The third parameter is a function that will handle the request
+* */
+router.addRoute('/unicorns', 'get', testGetRouteHandler);
+router.addRoute('/unicorns', 'patch', testPatchRouteHandler);
+
+/*
+* Start the server using the start method.
+* The first parameter is the port the server will use to listen for requests.
+* The second parameter is the router to use to process requests.
+**/
 server.start(3000, router.route);
